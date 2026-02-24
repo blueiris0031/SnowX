ec37c9f974df5ae0b0cbf78a16cbf0a1b0c58f8a
blueiris <blueiris_origin@outlook.com>
2026-02-24 12:16:49 +0800

    Update constants mapper

--- framework/constants/__mapper__.py
+++ framework/constants/__mapper__.py
@@ -1,5 +1,6 @@
 from . import callback
 from . import command_runner
+from . import framework
 from . import path
 from . import vmodule
 from ..constants.vmodule import VMODULE_ROOT_PATH, VMODULE_SUBROOT_PATH
@@ -10,5 +11,6 @@ adder = Adder(f"{VMODULE_ROOT_PATH.ROOT}.{VMODULE_SUBROOT_PATH.CONSTANTS}")
 
 adder.get_sub_adder("callback").auto_add(callback)
 adder.get_sub_adder("command_runner").auto_add(command_runner)
+adder.get_sub_adder("framework").auto_add(framework)
 adder.get_sub_adder("path").auto_add(path)
 adder.get_sub_adder("vmodule").auto_add(vmodule)
