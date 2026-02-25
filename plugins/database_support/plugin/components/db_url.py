1280b53da15f319737ccd59a32d6f4a13c913bfc
blueiris <blueiris_origin@outlook.com>
2026-02-25 19:31:19 +0800

    Add url generator manager

--- plugins/database_support/plugin/components/db_url.py
+++ plugins/database_support/plugin/components/db_url.py
@@ -5,10 +5,14 @@ class URLGeneratorManager:
     def __init__(self):
         self._url_generators: dict[str, Callable[..., str]] = {}
 
-    def add_type(self, db_type: str, generator: Callable[..., str]) -> None:
+    def add_generator(self, db_type: str, generator: Callable[..., str]) -> None:
         if db_type in self._url_generators:
-            raise Exception(f"DB type {db_type} already exists")
+            raise KeyError(f"DB type {db_type} already exists")
 
         self._url_generators[db_type] = generator
 
+    def get_generator(self, db_type: str) -> Callable[..., str]:
+        if db_type not in self._url_generators:
+            raise KeyError(f"DB type {db_type} does not exist")
 
+        return self._url_generators[db_type]
