5beec92a20bb55a50163b8ba05704999ab5854d7
blueiris <blueiris_origin@outlook.com>
2026-03-02 23:04:42 +0800

    Add stop_state to constants mapper

--- framework/constants/__mapper__.py
+++ framework/constants/__mapper__.py
@@ -2,6 +2,7 @@ from . import callback
 from . import command_runner
 from . import framework
 from . import path
+from . import stop_state
 from . import vmodule
 from ..constants.vmodule import VMODULE_ROOT_PATH, VMODULE_SUBROOT_PATH
 from ..kernel.vmodule.expand import Adder
@@ -13,4 +14,5 @@ adder.get_sub_adder("callback").auto_add(callback)
 adder.get_sub_adder("command_runner").auto_add(command_runner)
 adder.get_sub_adder("framework").auto_add(framework)
 adder.get_sub_adder("path").auto_add(path)
+adder.get_sub_adder("stop_state").auto_add(stop_state)
 adder.get_sub_adder("vmodule").auto_add(vmodule)
