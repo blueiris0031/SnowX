d4f49060533342d2cb905d072c7651a6ce132f23
blueiris <blueiris_origin@outlook.com>
2026-03-04 19:32:35 +0800

    Update async completion lock

--- framework/utils/lock.py
+++ framework/utils/lock.py
@@ -3,22 +3,22 @@ import asyncio
 
 class AsyncCompletionLock:
     def __init__(self):
-        self._runner_lock = asyncio.Event()
-        self._runner_lock.set()
+        self._event = asyncio.Event()
+        self._event.set()
 
         self._enter_count = 0
 
     async def __aenter__(self):
-        self._runner_lock.clear()
+        self._event.clear()
         self._enter_count += 1
 
     async def __aexit__(self, *_):
         self._enter_count = max(0, self._enter_count - 1)
         if self._enter_count == 0:
-            self._runner_lock.set()
+            self._event.set()
 
     async def wait(self):
-        await self._runner_lock.wait()
+        await self._event.wait()
 
 
 __all__ = [
