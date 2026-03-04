55e19aa82251b1e6347bd288b4c37ed03f95dd1c
blueiris <blueiris_origin@outlook.com>
2026-03-04 18:58:06 +0800

    Update mapper and main

--- framework/kernel/vmodule/mapper.py
+++ framework/kernel/vmodule/mapper.py
@@ -9,17 +9,12 @@ def auto_mapper() -> None:
     if _is_mapped:
         return
 
-    for subpath in (
-            Path(__file__)
-            .parent
-            .parent
-            .parent
-            .iterdir()
-    ):
+    base_path = Path(__file__).parent.parent.parent
+    for subpath in base_path.iterdir():
         if not subpath.is_dir():
             continue
         try:
-            importlib.import_module(f"{__package__}.{subpath.name}.__mapper__")
+            importlib.import_module(f"{base_path.name}.{subpath.name}.__mapper__")
         except ImportError:
             pass
 
