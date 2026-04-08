cdcf7d957d5fab8d8b57f920aef182e48a69720b
blueiris <blueiris_origin@outlook.com>
2026-04-08 22:25:35 +0800

    将engine_map重命名为generator_map

--- plugins/database_support/plugin/kernel/config.py
+++ plugins/database_support/plugin/kernel/config.py
@@ -6,19 +6,19 @@ from tortoise.config import ConnectionConfig
 
 
 class BaseConnectionsConfigGenerator(ABC):
-    _engine_map = {}
+    _generator_map = {}
 
     @classmethod
     def get_generator(cls, db_type: str) -> Optional[Type["BaseConnectionsConfigGenerator"]]:
-        return cls._engine_map.get(db_type, None)
+        return cls._generator_map.get(db_type, None)
 
     @classmethod
     def set_generator(cls, db_type: str, generator: Type["BaseConnectionsConfigGenerator"]) -> None:
-        cls._engine_map.setdefault(db_type, generator)
+        cls._generator_map.setdefault(db_type, generator)
 
     @classmethod
     def del_generator(cls, db_type: str) -> Optional[Type["BaseConnectionsConfigGenerator"]]:
-        cls._engine_map.pop(db_type, None)
+        cls._generator_map.pop(db_type, None)
 
     def __init_subclass__(cls, db_type: str) -> None:
         cls.set_generator(db_type, cls)
