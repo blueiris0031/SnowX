2f1851f4f94dc9bf4aa25c663f0c34695bd3dc93
blueiris0031 <blueiris_origin@outlook.com>
2026-04-16 15:57:03 +0800

    添加统一的reader

---
+++ plugins/snowx_config/plugin/components/reader/base_reader.py
@@ -0,0 +1,51 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Optional
+
+from snowx.api.logger import get_logger
+
+
+class BaseReader(ABC):
+    _type_map = {}
+    _logger = get_logger("SnowXConfigReader")
+
+    def __init_subclass__(cls, allow_type: str) -> None:
+        if allow_type not in cls._type_map:
+            cls._type_map[allow_type] = cls
+
+    @classmethod
+    def get_reader(cls, type_: str) -> Optional["BaseReader"]:
+        return cls._type_map.get(type_, None)
+
+    def s_read(self, path: Path) -> dict:
+        try:
+            self._logger.info(f"Trying to read [{path}]...")
+            result = self.read(path)
+            if not isinstance(result, dict):
+                raise TypeError(f'Reading result of the [{path}] is not a dict')
+
+            return result
+        except Exception as e:
+            self._logger.error(f"Failed to read [{path}], use empty dict.", exc_info=e)
+            return {}
+
+    def s_write(self, path: Path, data: dict) -> None:
+        try:
+            self._logger.info(f"Trying to write [{path}] ...")
+            if not isinstance(data, dict):
+                raise TypeError(f'Data to be written to the [{path}] is not a dict')
+
+            self.write(path, data)
+        except Exception as e:
+            self._logger.error(f"Failed to write [{path}].", exc_info=e)
+
+    @abstractmethod
+    def read(self, path: Path) -> dict: ...
+
+    @abstractmethod
+    def write(self, path: Path, data: dict) -> None: ...
+
+
+__all__ = [
+    "BaseReader",
+]
---
+++ plugins/snowx_config/plugin/components/reader/json_reader.py
@@ -0,0 +1,38 @@
+import json
+from pathlib import Path
+from typing import Any
+
+from .base_reader import BaseReader
+
+
+class JsonReader(BaseReader, allow_type="json"):
+    _r_default_read_kwargs = {}
+    _r_default_write_kwargs = {
+        "ensure_ascii": False,
+        "indent": 4,
+    }
+
+    def __init__(
+            self,
+            encoding: str = 'utf-8',
+            read_kwargs: dict[str, Any] | None = None,
+            write_kwargs: dict[str, Any] | None = None,
+    ):
+        self._encoding = encoding
+        if isinstance(read_kwargs, dict):
+            self._default_read_kwargs = {**self._r_default_read_kwargs, **read_kwargs}
+        if isinstance(write_kwargs, dict):
+            self._default_write_kwargs = {**self._r_default_write_kwargs, **write_kwargs}
+
+    def read(self, path: Path, **kwargs: Any) -> dict:
+        with open(path, "r", encoding=self._encoding) as f:
+            return json.load(f, **{**self._default_read_kwargs, **kwargs})
+
+    def write(self, path: Path, data: Any, **kwargs: Any) -> None:
+        with open(path, "w", encoding=self._encoding) as f:
+            json.dump(data, f, **{**self._default_write_kwargs, **kwargs})
+
+
+__all__ = [
+    "JsonReader",
+]
