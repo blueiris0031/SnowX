3e67266405638306081e0a11f0484cafad128e37
blueiris0031 <blueiris_origin@outlook.com>
2026-05-03 19:51:06 +0800

    优化数据库操作锁的实现

--- plugins/database_support/plugin/components/lock.py
+++ plugins/database_support/plugin/components/lock.py
@@ -1,36 +1,32 @@
 from typing import Any
 import asyncio
+from snowx.utils.lock import AsyncCompletionLock
 
 
-class SafeDBOperateLock:
-    def __init__(self, base_lock: Any | None = None) -> None:
+class SafeDBOperateLock(AsyncCompletionLock):
+    def __init__(self, base_lock: Any | None = None):
         self._db_lock = base_lock or asyncio.Lock()
 
         self._db_event = asyncio.Event()
         self._db_event.clear()
 
-        self._op_count = 0
-        self._op_event = asyncio.Event()
-        self._op_event.set()
+        super().__init__()
 
     async def __aenter__(self) -> None:
         await self._db_event.wait()
-        self._op_count += 1
-        self._op_event.clear()
+        await super().__aenter__()
         await self._db_lock.__aenter__()
 
     async def __aexit__(self, *args: Any, **kwargs: Any) -> None:
         await self._db_lock.__aexit__(*args, **kwargs)
-        self._op_count = max(self._op_count - 1, 0)
-        if self._op_count == 0:
-            self._op_event.set()
+        await super().__aexit__()
 
     async def set_init(self):
         self._db_event.set()
 
     async def set_close(self):
         self._db_event.clear()
-        await self._op_event.wait()
+        await super().wait()
 
 
 __all__ = [
