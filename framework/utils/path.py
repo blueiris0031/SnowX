904bb225d5cece3ba89dacc3bd04f0a52a3bc95a
blueiris0031 <blueiris_origin@outlook.com>
2026-05-06 22:26:18 +0800

    新增get_main_path

--- framework/utils/path.py
+++ framework/utils/path.py
@@ -1,4 +1,6 @@
+import __main__
 import re
+from pathlib import Path
 
 
 STRICT_PATTERN = re.compile(r'^[a-zA-Z][a-zA-Z0-9_]*[a-zA-Z0-9]$')
@@ -8,6 +10,15 @@ def is_valid_filename(filename: str) -> bool:
     return re.fullmatch(STRICT_PATTERN, filename) is not None
 
 
+def get_main_path() -> Path:
+    main_path = __main__.__file__
+    if main_path is None:
+        raise RuntimeError("Main program file does not exist")
+
+    return Path(main_path).resolve()
+
+
 __all__ = [
     "is_valid_filename",
+    "get_main_path",
 ]
