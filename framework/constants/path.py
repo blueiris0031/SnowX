dfd7a631fb49ad9267cd887ef3816669079c015b
blueiris0031 <blueiris_origin@outlook.com>
2026-05-06 22:26:43 +0800

    将cwd更换为get_main_path

--- framework/constants/path.py
+++ framework/constants/path.py
@@ -2,12 +2,12 @@ from dataclasses import dataclass
 from pathlib import Path
 
 from ..kernel.config import get_config
-from ..utils.path import is_valid_filename
+from ..utils.path import is_valid_filename, get_main_path
 
 
 @dataclass(frozen=True)
 class ConstantPath:
-    BASE: Path = Path.cwd()
+    BASE: Path = get_main_path().parent
     MAIN: Path = BASE / "framework"
     CONFIG: Path = BASE / get_config("CONFIG_DIR_NAME", "config", is_valid_filename)
     DATA: Path = BASE / get_config("DATA_DIR_NAME", "data", is_valid_filename)
